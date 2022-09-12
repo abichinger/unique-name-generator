fcff25378eefb6b4164b9ec1ff934861aa090f0c
Andreas Bichinger <andreas.bichinger@gmail.com>
2022-09-12 11:55:08 +0200

    feat: dart create

---
+++ example/unique_names_generator_example.dart
@@ -0,0 +1,6 @@
+import 'package:unique_names_generator/unique_names_generator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/unique_names_generator.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library unique_names_generator;
+
+export 'src/unique_names_generator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
